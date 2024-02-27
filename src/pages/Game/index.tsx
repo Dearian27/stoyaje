4dff4d5006e5b9c156fcbf05f6537b363bfa22d5
dearian <dearian2706@gmail.com>
2024-02-27 16:33:29 +0200

    redux

---
+++ src/pages/Game/index.tsx
@@ -0,0 +1,17 @@
+import Inputs from "../../components/Inputs";
+import Keyboard from "../../components/Keyboard";
+
+// import
+const word = "Rocket";
+const wordLength = word.length;
+
+const Game = () => {
+  return (
+    <main>
+      <Inputs />
+      <Keyboard />
+    </main>
+  );
+};
+
+export default Game;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <main>HOme</main>;
+};
+
+export default Home;
